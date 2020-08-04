32c49746a93d5643b1e8c3fdb943cad2a17e4f08
Peter Franklin <blk0akwhitera1n@gmail.com>
2020-08-04 15:12:25 -0500

    Updates

---
+++ precache-manifest.7c1d033a61bdf7dc4346789471d6c35e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dfb17acedba425bab425fac71a9499",
+    "url": "/pecan-tree-merc.0.0.1/index.html"
+  },
+  {
+    "revision": "33790ad7537a468a7ccb",
+    "url": "/pecan-tree-merc.0.0.1/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "2c0573d7b8ceccff9105",
+    "url": "/pecan-tree-merc.0.0.1/static/css/main.7bda77ac.chunk.css"
+  },
+  {
+    "revision": "33790ad7537a468a7ccb",
+    "url": "/pecan-tree-merc.0.0.1/static/js/2.e273e501.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pecan-tree-merc.0.0.1/static/js/2.e273e501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c0573d7b8ceccff9105",
+    "url": "/pecan-tree-merc.0.0.1/static/js/main.a7186a85.chunk.js"
+  },
+  {
+    "revision": "13e502e8704b57e63c6b",
+    "url": "/pecan-tree-merc.0.0.1/static/js/runtime-main.90d442f7.js"
+  }
+]);
