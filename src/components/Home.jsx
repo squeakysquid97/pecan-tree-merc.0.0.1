2b9518f58473654fbbdb69bab26e0ca3c0b73086
Peter Franklin <47440135+squeakysquid97@users.noreply.github.com>
2022-06-25 14:41:18 -0500

    Additional changes for pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
               target="_blank"
               rel="noopener"
             >
-              <img className="w-25" src="/amazonLogo.png" alt="" />
+              <img className="w-25" src="./amazonLogo.png" alt="" />
             </a>
           </div>
         </div>
